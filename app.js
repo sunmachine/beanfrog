ca974e9936f1a948bb916c5a452d0c80f1b3e7d6
Dan Peavey <danpeavey@gmail.com>
2021-11-27 01:15:44 -0800

    Extensible event pattern implementation.

--- app.js
+++ app.js
@@ -15,24 +15,31 @@ const client = new Client({
     intents: [Intents.FLAGS.GUILDS]
 });
 
-// Set up client commands
+// Command registration.
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', (c) => {
-    console.log(`Ready: ${c.user.tag}`);
-});
+// Event registration.
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
+// Handle commands.
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
+
     if (!command) return;
 
     try {
